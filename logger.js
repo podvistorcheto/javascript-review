43ff8a0bdb0ff6fbdac1cf073eddac9e8b1c7e65
podvistorcheto <denev.s.deyan@gmail.com>
2021-02-17 16:26:10 +0000

    Node.js review part 3 http

--- logger.js
+++ logger.js
@@ -8,4 +8,13 @@ class Logger extends EventEmitter {
     }
 }
 
-module.exports = Logger
+// module.exports = Logger
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hello Node.js');
+logger.log('Hello Deyan');
