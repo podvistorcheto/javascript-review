a929bf6e2491ab65a61f60fec7bbe96f9feda188
podvistorcheto <denev.s.deyan@gmail.com>
2021-02-17 10:58:14 +0000

    Node.js review part 1

---
+++ reference/os_demo.js
@@ -0,0 +1,16 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+//CPU Arch
+console.log(os.arch());
+
+//CPU Core Info
+//console.log(os.cpus());
+
+//home directory
+console.log(os.homedir());
+
+// Uptime or second the system is been up
+console.log(os.uptime());
