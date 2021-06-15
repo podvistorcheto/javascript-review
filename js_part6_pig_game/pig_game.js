91b1b0defe605a1e7fb8dce2a2fdb98198b41c99
podvistorcheto <denev.s.deyan@gmail.com>
2021-06-15 15:01:01 +0000

    JS Part 6 - added reset score and change player functionality w ith if statement

--- js_part6_pig_game/pig_game.js
+++ js_part6_pig_game/pig_game.js
@@ -5,7 +5,6 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-
 // target the dice element
 const diceEl = document.querySelector('.dice');
 // make the buttons move
@@ -16,14 +15,17 @@ const btnHold = document.querySelector('.btn-hold');
 // set the default scores for both players to zero when starting the game
 score0El.textContent = 0;
 score1El.textContent = 0;
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 // set the dice not to be shown before is rolled first time
 diceEl.classList.add('hidden');
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function() {
     // generate random dice number
-    let dice = Math.trunc(Math.random() * 6 + 1);
+    const dice = Math.trunc(Math.random() * 6 + 1);
     console.log(dice);
     // display dice number
     diceEl.classList.remove('hidden');
@@ -32,8 +34,10 @@ btnRoll.addEventListener('click', function() {
     if(dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
-    } else{
-
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        // Swithc to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1: 0;
     }
-})
+});
