d2b2c98716b38a706723674289bfe4ed3ce30140
podvistorcheto <denev.s.deyan@gmail.com>
2021-06-15 14:25:53 +0000

    JS Part 6 - save current score functionality

--- js_part6_pig_game/pig_game.js
+++ js_part6_pig_game/pig_game.js
@@ -1,13 +1,39 @@
 'use strict';
 
-// set the var for the scores to selecting elements
+// target the selected elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
+// target the dice element
+const diceEl = document.querySelector('.dice');
+// make the buttons move
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn-hold');
+
+// set the default scores for both players to zero when starting the game
 score0El.textContent = 0;
 score1El.textContent = 0;
+let currentScore = 0;
+// set the dice not to be shown before is rolled first time
 diceEl.classList.add('hidden');
 
+// rolling dice functionality
+btnRoll.addEventListener('click', function() {
+    // generate random dice number
+    let dice = Math.trunc(Math.random() * 6 + 1);
+    console.log(dice);
+    // display dice number
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // check for rolled 1: if true, switch to next players
+    if(dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // change later
+    } else{
 
-
+    }
+})
