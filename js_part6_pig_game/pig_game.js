e411188a3ec4c076d1a5b8a52a80f3d65c726cc4
podvistorcheto <denev.s.deyan@gmail.com>
2021-06-15 07:42:16 +0000

    JS Part 6 Roll Dice Pig Game starter code

---
+++ js_part6_pig_game/pig_game.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// set the var for the scores to selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
