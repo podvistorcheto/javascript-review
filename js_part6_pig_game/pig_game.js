7abffe057774b75f5d35eedbacd2006af884eac5
podvistorcheto <denev.s.deyan@gmail.com>
2021-06-16 12:18:29 +0000

    JS Part 6 - Player switch styles added

--- js_part6_pig_game/pig_game.js
+++ js_part6_pig_game/pig_game.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // target the selected elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -39,5 +41,8 @@ btnRoll.addEventListener('click', function() {
         // Swithc to next player
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         activePlayer = activePlayer === 0 ? 1: 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 });
