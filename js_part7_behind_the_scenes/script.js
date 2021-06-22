074eb67bcb75381c4b55f433f8608037003a7559
podvistorcheto <denev.s.deyan@gmail.com>
2021-06-22 14:26:30 +0000

    Hoisting variables and TDZ examples

--- js_part7_behind_the_scenes/script.js
+++ js_part7_behind_the_scenes/script.js
@@ -1,5 +1,5 @@
 // 'use strict';
-
+/*
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
 
@@ -37,3 +37,39 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+
+// HOISTING AND TDZ IN PRACTICE
+//hoisting with variables
+console.log(me); // logs undefined because is declared with var
+//console.log(job); // error cannot access it because is in the TDZ
+//console.log(year); // same error cannot access it because is in the TDZ
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// hoisting with functions
+console.log(addDecl(2,3));
+//console.log(addExpr(2,3));
+//console.log(addArrow(2,3));
+
+function addDecl(a,b) {
+    return a + b;
+};
+
+const addExpr = function(a,b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+*/
+// Example
+if(!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted');
+}
+
