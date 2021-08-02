cce733bbb7bb4d8f4fb87c2c09ad20c5f9b2cbb7
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-02 13:58:09 +0000

    game 1.2 logic added

--- part_3_review/script.js
+++ part_3_review/script.js
@@ -27,12 +27,22 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Bravo! Correct number!";
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too High";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
