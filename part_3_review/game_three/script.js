07d87def3aebe97c1d961ae759a2de1a4cfd7e70
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-03 07:46:53 +0000

    game 3 initialised

---
+++ part_3_review/game_three/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Selecting elements
+const score1Element = document.querySelector("#score--0");
+const score2Element = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+score1Element.textContent = 0;
+score2Element.textContent = 0;
+diceElement.classList.add("hidden");
