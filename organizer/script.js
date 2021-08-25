23cf271559bca33b20c8da6cbd1a7f1fa43bf911
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-25 12:45:43 +0000

    Making the add button class active

--- organizer/script.js
+++ organizer/script.js
@@ -1,4 +1,18 @@
 "use script";
 
-const test = true;
-console.log(test);
+// const test = true;
+// console.log(test);
+
+// defining the elements
+const addTaskBox = document.querySelector(".addNewTask input");
+const addTaskBtn = document.querySelector(".addNewTask button");
+
+addTaskBox.onkeyup = () => {
+  let userInput = addTaskBox.value; // gets the users input
+  if (userInput.trim() != 0) {
+    // if user input not blank
+    addTaskBtn.classList.add("active");
+  } else {
+    addTaskBtn.classList.remove("active");
+  }
+};
