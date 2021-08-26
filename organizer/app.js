d2fb249dd2cdaa2bb6e8c50e01931d0b11d048be
podvistorcheto <denev.s.deyan@gmail.com>
2021-08-26 09:44:31 +0000

    Method to add task triggered for the console

--- organizer/app.js
+++ organizer/app.js
@@ -3,6 +3,16 @@
 // const test = true;
 // console.log(test);
 
+const database = {
+  todoItems: [
+    { id: 0, name: "Get Groceries", description: "Do with a shopping list" },
+    { id: 1, name: "Run and exercise", description: "For at least 45 mins" },
+    { id: 2, name: "Build a to-do app", description: "Use JavaScript" },
+  ],
+  currenTask: null,
+  totalToDoItems: 0,
+};
+
 // 4.LOCAL STORAGE CONFIGURATION - to be added at the end
 
 // 1. TASKS MANAGER
@@ -14,7 +24,7 @@ const taskManager = (function () {
   };
   // data structure
   const database = {
-    todoitems: [
+    todoItems: [
       { id: 0, name: "Get Groceries", description: "Do with a shopping list" },
       { id: 1, name: "Run and exercise", description: "For at least 45 mins" },
       { id: 2, name: "Build a to-do app", description: "Use JavaScript" },
@@ -24,22 +34,65 @@ const taskManager = (function () {
   };
   // return function to be able to read the data with a public method
   return {
+    // get the items
+    getTasks: function () {
+      return database.todoItems;
+    },
     logData: function () {
       return database;
     },
   };
 })();
-// 2. USER CONTROLLER
+// 2. UI CONTROLLER
 const userControl = (function () {
+  const userSelectors = {
+    listWithTasks: ".task-list",
+    addTaskBtn: ".add-new-task",
+  };
   // public method
-  return {};
+  return {
+    fillTaskList: function (todoItems) {
+      let html = "";
+
+      todoItems.forEach(function (item) {
+        html += `<li> id="item-${item.id}"> ${item.name}
+      <i class="fas fa-edit"></i> <i class="fas fa-trash"></i></span>
+      </li>`;
+      });
+      // Insert list names
+      document.querySelector(userSelectors.listWithTasks).innerHTML = html;
+    },
+    getSelectors: function () {
+      return userSelectors;
+    },
+  };
 })();
 // 3. APP MANAGER
 const appManager = (function (taskManager, userControl) {
+  // Load events
+  const loadEvents = function () {
+    // get user selectors
+    const userSelectors = userControl.getSelectors();
+    // event which adds task
+    document
+      .querySelector(userSelectors.addTaskBtn)
+      .addEventListener("click", addTaskSubmit);
+  };
+  // Add task submit method
+  const addTaskSubmit = function (e) {
+    console.log("Adding Task....");
+    e.preventDefault();
+  };
   // public methods
   return {
     startApp: function () {
       console.log("Initializing app....");
+      // fetch items from database
+      const todoItems = taskManager.getTasks();
+      // Fill in the list with items
+      userControl.fillTaskList(todoItems);
+      // load events function
+      loadEvents();
     },
   };
 })(taskManager, userControl);
